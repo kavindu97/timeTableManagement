a3ec62414283618f1cab2c5147cd894e6aac89a1
Kavindu Ariyasinghe <kavindumalshan@gmail.com>
2020-09-11 22:29:19 +0530

    fix-issue

--- src/app/main.js
+++ src/app/main.js
@@ -8,75 +8,17 @@ const Working = require("./models/timeManagement/Working");
 function createWindow() {
   const win = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   win.loadFile("src/app/index.html");
 }
 
-// ipcMain.on("new-task", async (e, arg) => {
-//   const newTask = new Task(arg);
-//   const taskSaved = await newTask.save();
-//   console.log(taskSaved);
-//   e.reply("new-task-created", JSON.stringify(taskSaved));
-// });
-
-// ipcMain.on("get-tasks", async (e, arg) => {
-//   const tasks = await Task.find();
-//   e.reply("get-tasks", JSON.stringify(tasks));
-// });
-
-// ipcMain.on("delete-task", async (e, args) => {
-//   const taskDeleted = await Task.findByIdAndDelete(args);
-//   e.reply("delete-task-success", JSON.stringify(taskDeleted));
-// });
-
-// ipcMain.on("update-task", async (e, args) => {
-//   console.log(args);
-//   const updatedTask = await Task.findByIdAndUpdate(
-//     args.idTaskToUpdate,
-//     { name: args.name, description: args.description },
-//     { new: true }
-//   );
-//   e.reply("update-task-success", JSON.stringify(updatedTask));
-// });
-
-
-//Tag - Sprint-01
-
-// ipcMain.on("new-tag", async (e, arg) => {
-//   const newTag = new Tag(arg);
-//   const tagSaved = await newTag.save();
-//   console.log(tagSaved);
-//   e.reply("new-tag-created", JSON.stringify(tagSaved));
-// });
-
-// ipcMain.on("get-tags", async (e, arg) => {
-//   const tags = await Tag.find();
-//   e.reply("get-tags", JSON.stringify(tags));
-// });
-
-// ipcMain.on("delete-tag", async (e, args) => {
-//   const tagDeleted = await Tag.findByIdAndDelete(args);
-//   e.reply("delete-tag-success", JSON.stringify(tagDeleted));
-// });
-
-// ipcMain.on("update-tag", async (e, args) => {
-//   console.log(args);
-//   const updatedTag = await Task.findByIdAndUpdate(
-//     args.idTagToUpdate,
-//     { tagCategory: args.tagCategory, batch: args.batch, group: args.group, subject: args.subject,
-//       startingTime: args.startingTime, endingTime: args.endingTime, note: args.note  },
-//     { new: true }
-//   );
-//   e.reply("update-tag-success", JSON.stringify(updatedTag));
-// });
-
 //new Update Tag
 
 ipcMain.on("new-tag", async (e, arg) => {
-  console.log("newtag")
+  console.log("newtag");
   const newTag = new Tag(arg);
   const tagSaved = await newTag.save();
   console.log(tagSaved);
@@ -94,26 +36,25 @@ ipcMain.on("delete-tag", async (e, args) => {
 });
 
 ipcMain.on("update-tag", async (e, args) => {
-  console.log(args+"dghdfh");
+  console.log(args + "dghdfh");
   const updatedTag = await Tag.findByIdAndUpdate(
     args.idTagToUpdate,
-    { category: args.category, batch: args.batch, group: args.group, subject: args.subject,
-      startingTime: args.startingTime, endingTime: args.endingTime, note: args.note },
+    {
+      category: args.category,
+      batch: args.batch,
+      group: args.group,
+      subject: args.subject,
+      startingTime: args.startingTime,
+      endingTime: args.endingTime,
+      note: args.note,
+    },
     { new: true }
   );
   e.reply("update-tag-success", JSON.stringify(updatedTag));
 });
 
-
-
-
-
-
-
-
-
 ipcMain.on("new-student", async (e, arg) => {
-  console.log("newstudent")
+  console.log("newstudent");
   const newStudent = new Student(arg);
   const studentSaved = await newStudent.save();
   console.log(studentSaved);
@@ -134,24 +75,19 @@ ipcMain.on("update-student", async (e, args) => {
   console.log(args);
   const updatedStudent = await Student.findByIdAndUpdate(
     args.idStudentToUpdate,
-    { acedemicYear: args.acedemicYear, programme: args.programme, groupCount: args.groupCount, subGroupCount: args.subGroupCount},
+    {
+      acedemicYear: args.acedemicYear,
+      programme: args.programme,
+      groupCount: args.groupCount,
+      subGroupCount: args.subGroupCount,
+    },
     { new: true }
   );
   e.reply("update-student-success", JSON.stringify(updatedStudent));
 });
 
-
-
-
-
-
-
-
-
-
-
 ipcMain.on("new-time30", async (e, arg) => {
-  console.log("newtime30")
+  console.log("newtime30");
   const newTime30 = new Time30(arg);
   const time30Saved = await newTime30.save();
   console.log(time30Saved);
@@ -172,22 +108,14 @@ ipcMain.on("update-time30", async (e, args) => {
   console.log(args);
   const updatedTime30 = await Time30.findByIdAndUpdate(
     args.idTime30ToUpdate,
-    { startingTime30: args.startingTime30, endingTime30: args.endingTime30},
+    { startingTime30: args.startingTime30, endingTime30: args.endingTime30 },
     { new: true }
   );
   e.reply("update-time30-success", JSON.stringify(updatedTime30));
 });
 
-
-
-
-
-
-
-
-
 ipcMain.on("new-time60", async (e, arg) => {
-  console.log("newtime60")
+  console.log("newtime60");
   const newTime60 = new Time60(arg);
   const time60Saved = await newTime60.save();
   console.log(time60Saved);
@@ -208,33 +136,22 @@ ipcMain.on("update-time60", async (e, args) => {
   console.log(args);
   const updatedTime60 = await Time60.findByIdAndUpdate(
     args.idTime60ToUpdate,
-    { startingTime60: args.startingTime60, endingTime60: args.endingTime60},
+    { startingTime60: args.startingTime60, endingTime60: args.endingTime60 },
     { new: true }
   );
   e.reply("update-time60-success", JSON.stringify(updatedTime60));
 });
 
-
-
-
-
-
-
-
-
-
-
 ipcMain.on("new-working", async (e, arg) => {
-  console.log("1")
+  console.log("1");
   const newWorking = new Working(arg);
   const workingSaved = await newWorking.save();
   console.log(workingSaved);
   e.reply("new-working-created", JSON.stringify(workingSaved));
 });
 
-
 ipcMain.on("new-time60", async (e, arg) => {
-  console.log("newtime60")
+  console.log("newtime60");
   const newTime60 = new Time60(arg);
   const time60Saved = await newTime60.save();
   console.log(time60Saved);
@@ -255,36 +172,21 @@ ipcMain.on("update-working", async (e, args) => {
   console.log(args);
   const updatedWorking = await Working.findByIdAndUpdate(
     args.idWorkingToUpdate,
-    { numberOfWorking: args.numberOfWorking, monday: args.monday, tuesday: args.tuesday, wednesday: args.wednesday, thursday: args.thursday, friday: args.friday, saturday: args.saturday, sunday: args.sunday, hours: args.hours, minutes: args.minutes},
+    {
+      numberOfWorking: args.numberOfWorking,
+      monday: args.monday,
+      tuesday: args.tuesday,
+      wednesday: args.wednesday,
+      thursday: args.thursday,
+      friday: args.friday,
+      saturday: args.saturday,
+      sunday: args.sunday,
+      hours: args.hours,
+      minutes: args.minutes,
+    },
     { new: true }
   );
   e.reply("update-working-success", JSON.stringify(updatedWorking));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = { createWindow };
